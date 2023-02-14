36cab51dedcd92ada727ac48caffd2bf9b9dda7b
Aswin <aswinkrishna363@gmail.com>
2023-02-14 11:50:00 +0530

    bugg fixed

--- script.js
+++ script.js
@@ -5,52 +5,52 @@ const mole = document.querySelector('.mole')
 
 let hitPosition
 let result = 0
-let currentTime = 60
+let currentTime = 10
+let timerId = null
 
-function randomSquare() {
+// to make random square
+const randomSquare = () => {
   squares.forEach(square => {
     square.classList.remove('mole')
   })
 
   let randomSquare = squares[Math.floor(Math.random() * 9)]
   randomSquare.classList.add('mole')
-
   hitPosition = randomSquare
-  
 }
 
-
+// update the result
 squares.forEach(square => {
   square.addEventListener('mousedown', () => {
     
     if(square.id == hitPosition.id) {
       result++
       score.innerText = result
-      console.log(result)
+      // console.log(result)
       hitPosition = null
     }
   })
 })
 
-function moveMole() {
-  let timerId = null
+// to moving the mole 
+const moveMole = () => {
   timerId = setInterval(randomSquare, 1000)
 }
 
-
 moveMole()
 
+// to calculate the timing
 function countDown() {
-  currentTime--
-  time.innerText = currentTime
-
-  if(currentTime == 0 ) {
+  console.log(currentTime)
+  if(currentTime <= 0 ) {
     clearInterval(counttDownTimer)
+    clearInterval(timerId)
     alert('GAME OVER !! Your score is ' + result)
-
+  } else {
+    currentTime--
+    time.innerText = currentTime
   }
-
 }
 
-let counttDownTimer =setInterval(countDown, 1000)
+let counttDownTimer = setInterval(countDown, 1000)
 // randomSquare()
