a2c328f773133ce40f614b9c512bc5eaf9b973ed
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-08-02 20:36:13 +0530

    Everything related  to format

--- script.js
+++ script.js
@@ -1,82 +1,78 @@
-
-// only load after 500ms when screen loads
+// Only load after 500ms when the screen loads
 const windowLoad = () => {
-  const score = document.getElementById('score')
-  const time = document.getElementById('time_left')
-  const squares = document.querySelectorAll('.square')
-  const mole = document.querySelector('.mole')
+  const score = document.getElementById('score');
+  const time = document.getElementById('time_left');
+  const squares = document.querySelectorAll('.square');
+  const mole = document.querySelector('.mole');
 
-  let hitPosition
-  let result = 0
-  let currentTime = 30
-  let timerId = null
+  let hitPosition;
+  let result = 0;
+  let currentTime = 30;
+  let timerId = null;
 
-  // to make random square
+  // To make a random square
   const randomSquare = () => {
-    squares.forEach(square => {
-      square.classList.remove('mole')
-    })
+    squares.forEach((square) => {
+      square.classList.remove('mole');
+    });
 
-    let randomSquare = squares[Math.floor(Math.random() * 9)]
-    randomSquare.classList.add('mole')
-    hitPosition = randomSquare
-  }
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('mole');
+    hitPosition = randomSquare;
+  };
 
-  // update the result
-  squares.forEach(square => {
+  // Update the result
+  squares.forEach((square) => {
     square.addEventListener('mousedown', () => {
-      
-      if(square.id == hitPosition.id) {
-        result++
-        score.innerText = result
-        document.getElementById('catch').play()
+      if (square.id == hitPosition.id) {
+        result++;
+        score.innerText = result;
+        document.getElementById('catch').play();
         // console.log(result)
-        hitPosition = null
+        hitPosition = null;
       }
-    })
-  })
+    });
+  });
 
-  // to moving the mole 
+  // To move the mole
   const moveMole = () => {
-    timerId = setInterval(randomSquare, 700)
-  }
+    timerId = setInterval(randomSquare, 700);
+  };
 
-  moveMole()
+  moveMole();
 
-  // to calculate the timing
+  // To calculate the timing
   const countDown = () => {
-    console.log(currentTime)
-    if(currentTime <= 0 ) {
-      clearInterval(counttDownTimer)
-      clearInterval(timerId)
-      alert('GAME OVER !! Your score is ' + result)
+    console.log(currentTime);
+    if (currentTime <= 0) {
+      clearInterval(countDownTimer);
+      clearInterval(timerId);
+      alert('GAME OVER !! Your score is ' + result);
     } else {
-      currentTime--
-      time.innerText = currentTime
+      currentTime--;
+      time.innerText = currentTime;
     }
-  }
-  let counttDownTimer = setInterval(countDown, 1000)
-  // randomSquare()
+  };
 
-}
+  let countDownTimer = setInterval(countDown, 1000);
+  // randomSquare();
+};
+
 window.onload = () => {
-  setTimeout(windowLoad, 500)
-}
+  setTimeout(windowLoad, 500);
+};
 
-
-
-
-// scrolling effects
+// Scrolling effects
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry)
-    if(entry.isIntersecting) {
-      entry.target.classList.add ('show')
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
     } else {
-      entry.target.classList.remove('show')
+      entry.target.classList.remove('show');
     }
-  })
-})
+  });
+});
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) => observer.observe(el))
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
